95f51ecf78f4a7b502385d420c49068a929145a3
bhaves19h <162792561+bhaves19h@users.noreply.github.com>
2024-08-23 00:05:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ document.addEventListener("scroll", function() {
         }
     });
 });
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId   
+ = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth'   
+ });
+    });
+});
